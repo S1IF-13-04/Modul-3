512b32af86037c9d49e00dea3473ab084c794b30
Zhafif-Yusuf <145468227+Zhafif-Yusuf@users.noreply.github.com>
2025-10-04 15:28:49 +0700

    Add files via upload

---
+++ Zhafif Yusuf Al Amin/guided2.go
@@ -0,0 +1,16 @@
+	package main
+
+	import "fmt"
+
+	func main() {
+		var a, t int
+		
+		fmt.Print("Input alas: ")
+		fmt.Scanln(&a)
+		fmt.Print("Input tinggi: ")
+		fmt.Scanln(&t)
+
+		v :=  (a * t )/2
+		var f = float64(v)
+		fmt.Print(f + 0.5)
+	}
