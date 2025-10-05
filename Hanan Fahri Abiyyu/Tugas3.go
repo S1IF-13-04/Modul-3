94234e4d658be98446355e34debe1afc09044729
Hanan Fahri Abiyyu <hananfahri78@gmail.com>
2025-10-05 16:41:37 +0700

    Tugas3.go

---
+++ Hanan Fahri Abiyyu/Tugas3.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var tahun int
+	var kabisat bool
+
+	fmt.Print("Masukkan Tahun :")
+	fmt.Scanln(&tahun)
+
+	kabisat = (tahun%400 == 0) || (tahun%4 == 0 && tahun%100 != 0)
+	fmt.Println("Kabisat :", kabisat)
+}
