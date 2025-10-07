1d5f4c2eb518691409b8bab8ba4015af08b18c4b
Allsche <138831900+Allsche@users.noreply.github.com>
2025-10-08 04:42:07 +0700

    Add files via upload

---
+++ Dadi Maulana Muhammad/Source Code/assignment-01.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x float64
+	fmt.Scan(&x)
+
+	f := 2/(x+5) + 5
+
+	fmt.Print(f)
+}
---
+++ Dadi Maulana Muhammad/Source Code/assignment-04.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var c float64
+	fmt.Print("Temperatur Celsius: ")
+	fmt.Scan(&c)
+
+	r := c * 4 / 5
+	f := c*9/5 + 32
+	k := c + 273
+
+	fmt.Printf("Derajat Reamur: %.0f\n", r)
+	fmt.Printf("Derajat Fahrenheit: %.0f\n", f)
+	fmt.Printf("Derajat Kelvin: %.0f\n", k)
+}
